ba77935e1a8c4f9b43da03d9e1c87955d02c6fde
geekplux <geekplux@qq.com>
2015-12-07 01:55:33 +0800

    computte ja ra pa

--- basic_alogrithm/link-precision.py
+++ basic_alogrithm/link-precision.py
@@ -23,6 +23,11 @@ test_list = list()
 pp = pprint.PrettyPrinter(indent=4)
 
 
+
+
+
+
+
 def find_file(file_name):
     cwd = os.getcwd()
 
@@ -63,25 +68,80 @@ def divide(sample_list):
             test_list.append(u)
 
 
+def generate_graph():
+    G.add_edges_from(universe)
+    # tG.add_edges_from(train_list)
+    # pG.add_edges_from(test_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+def jaccard_coefficient():
+    _G = copy.deepcopy(G)
+    jc = nx.jaccard_coefficient(_G)
+    _G_without_preset = _G.remove_edges_from(test_list)
+    jc_without_preset = nx.jaccard_coefficient(_G_without_preset)
+    compute_auc(jc, jc_without_preset)
+    compute_precision(jc, jc_without_preset)
+
+
+def resource_allocation():
+    _G = copy.deepcopy(G)
+    ra = nx.resource_allocation_index(_G)
+    _G_without_preset = _G.remove_edges_from(test_list)
+    ra_without_preset = nx.resource_allocation(_G_without_preset)
+    compute_auc(ra, ra_without_preset)
+    compute_precision(ra, ra_without_preset)
+
+
+def preferential_attachment():
+    _G = copy.deepcopy(G)
+    pa = nx.preferential_attachment(_G)
+    _G_without_preset = _G.remove_edges_from(test_list)
+    pa_without_preset = nx.preferential_attachment(_G_without_preset)
+    compute_auc(pa, pa_without_preset)
+    compute_precision(pa, pa_without_preset)
+
+
+
+
+
+
+
+
+
+
+
+
+
 def draw(sample_list):
     _universe_copy = copy.deepcopy(universe)
     for i, u in enumerate(_universe_copy):
         if i in sample_list:
             u.append({'test': 1})
 
-    G.add_edges_from(_universe_copy)
+    _G.add_edges_from(_universe_copy)
 
 
-    etrain=[(u,v) for (u,v,d) in G.edges(data=True) if 'test' not in d]
-    etest=[(u,v) for (u,v,d) in G.edges(data=True) if 'test' in d and d['test']==1]
+    etrain=[(u,v) for (u,v,d) in _G.edges(data=True) if 'test' not in d]
+    etest=[(u,v) for (u,v,d) in _G.edges(data=True) if 'test' in d and d['test']==1]
 
-    pos=nx.spring_layout(G) # positions for all nodes
-    nx.draw_networkx_nodes(G, pos, node_size=200)
+    pos=nx.spring_layout(_G) # positions for all nodes
+    nx.draw_networkx_nodes(_G, pos, node_size=200)
     # edges
-    nx.draw_networkx_edges(G, pos, edgelist=etrain, width=2)
-    nx.draw_networkx_edges(G, pos, edgelist=etest, width=2, alpha=0.5, edge_color='b', style='dashed')
+    nx.draw_networkx_edges(_G, pos, edgelist=etrain, width=2)
+    nx.draw_networkx_edges(_G, pos, edgelist=etest, width=2, alpha=0.5, edge_color='b', style='dashed')
 
-    nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
+    nx.draw_networkx_labels(_G, pos, font_size=10, font_family='sans-serif')
 
     plt.axis('off')
     plt.savefig("graph.png") # save as png
@@ -89,9 +149,8 @@ def draw(sample_list):
 
 
 
-def generate_graph():
-    tG.add_edges_from(train_list)
-    pG.add_edges_from(test_list)
+
+
 
 
 
@@ -101,4 +160,5 @@ read_file(path)
 sample_list = sampling(20)
 divide(sample_list)
 generate_graph()
-draw(sample_list)
+# draw(sample_list)
+
