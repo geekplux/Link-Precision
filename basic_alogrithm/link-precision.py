2a9ac910e8d4cea849004aebb2dd033f192d8e53
geekplux <geekplux@qq.com>
2015-12-07 02:59:13 +0800

    compute auc

--- basic_alogrithm/link-precision.py
+++ basic_alogrithm/link-precision.py
@@ -82,7 +82,26 @@ def generate_graph():
 
 
 def compute_auc(s, s_w):
-    pass
+    test_score = list()
+    _auc_score = 0.0
+    for u, v, p in s:
+        for _u, _v in test_list:
+            if u == _u and v == _v:
+                test_score.append([u, v, p])
+
+    for u, v, p in s_w:
+        for _u, _v, _p in test_score:
+            if p > _p:
+                _auc_score += 0.0
+            elif p < _p:
+                _auc_score += 1.0
+            else:
+                _auc_score += 0.5
+
+
+    _auc = _auc_score / (len(train_list) * len(test_list))
+
+    return _auc
 
 
 def compute_precision(s, s_w):
@@ -102,27 +121,27 @@ def jaccard_coefficient():
     _G = copy.deepcopy(G)
     jc = nx.jaccard_coefficient(_G)
     _G.remove_edges_from(test_list)
-    jc_without_preset = nx.jaccard_coefficient(_G)
-    compute_auc(jc, jc_without_preset)
-    # compute_precision(jc, jc_without_preset)
+    jc_without_test_set = nx.jaccard_coefficient(_G)
+
+    return (jc, jc_without_test_set)
 
 
 def resource_allocation():
     _G = copy.deepcopy(G)
     ra = nx.resource_allocation_index(_G)
     _G.remove_edges_from(test_list)
-    ra_without_preset = nx.resource_allocation(_G)
-    compute_auc(ra, ra_without_preset)
-    compute_precision(ra, ra_without_preset)
+    ra_without_test_set = nx.resource_allocation_index(_G)
+
+    return (ra, ra_without_test_set)
 
 
 def preferential_attachment():
     _G = copy.deepcopy(G)
     pa = nx.preferential_attachment(_G)
     _G.remove_edges_from(test_list)
-    pa_without_preset = nx.preferential_attachment(_G)
-    compute_auc(pa, pa_without_preset)
-    compute_precision(pa, pa_without_preset)
+    pa_without_test_set = nx.preferential_attachment(_G)
+
+    return (pa, pa_without_test_set)
 
 
 
@@ -170,9 +189,17 @@ def draw(sample_list):
 # main
 path = find_file(file_name)
 read_file(path)
+
+
 sample_list = sampling(20)
 divide(sample_list)
 generate_graph()
-jaccard_coefficient()
-# draw(sample_list)
+ja = jaccard_coefficient()
+auc_by_ja = compute_auc(ja[0], ja[1]) # ja[1] is ja score without test set
+ra = resource_allocation()
+auc_by_ra = compute_auc(ra[0], ra[1]) # ra[1] is ra score without test set
+pa = preferential_attachment()
+auc_by_pa = compute_auc(pa[0], pa[1]) # pa[1] is pa score without test set
+print(auc_by_ja, auc_by_ra, auc_by_pa)
 
+# draw(sample_list)
